ccb13792cb78c8326a8b7fca886b7d73bc45fbf5
Evilcuted <e0147977@u.nus.edu>
2017-04-11 14:29:49 +0800

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.template')
+@section('title')
+articleplus
+@stop
+@section('main')
+<h1>sd</h1>
+@stop
---
+++ resources/views/nav.blade.php
@@ -0,0 +1,36 @@
+<nav class="brand-header">
+  <div class="navbar navbar-default">
+    <div class="container fluid">
+      <div class="navbar-header">
+
+        <a class="navbar-brand" href="/">
+          <i class="glyphicon glyphicon-heart-empty"></i> SkillXchg
+        </a>
+
+        <button class="navbar-toggle collapsed" data-target="#navcol-1" data-toggle="collapse">
+          <span class="sr-only">
+              Toggle navigation
+          </span>
+          <span class="icon-bar">
+          </span>
+          <span class="icon-bar">
+          </span>
+          <span class="icon-bar">
+          </span>
+        </button>
+      </div>
+      <!-- end navbar-header -->
+      <div class="collapse navbar-collapse" id="navcol-1">
+        <ul class="nav navbar-nav navbar-left">
+          <li><a href="s" class="navbar-link">Browse Posts</a></li>
+          <li><a href="s" class="navbar-link">People</a></li>
+        </ul>
+
+      </div>
+      <!-- end of navbar-collapse -->
+    </div>
+    <!-- end of nav container -->
+  </div>
+  <!-- end of navbar-default -->
+</nav>
+
